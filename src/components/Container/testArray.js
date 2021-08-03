f1a5ecee9cc5cbf462b6c17ef29b2295a25e3402
Oleksandr <qwertyuiop.ua@gmail.com>
2021-08-03 22:11:35 +0300

    commit

---
+++ src/components/Container/testArray.js
@@ -0,0 +1,29 @@
+const testArray = [
+    {
+        id: 1,
+        name: 'iPhone XS',
+    },
+    {
+        id: 2,
+        name: 'iPhone S',
+    },
+    {
+        id: 3,
+        name: 'iPhone 3',
+    },
+
+    {
+        id: 4,
+        name: 'iPhone 4',
+    },
+    {
+        id: 5,
+        name: 'iPhone 5',
+    },
+    {
+        id: 6,
+        name: 'iPhone 6',
+    },
+]
+
+export default testArray
