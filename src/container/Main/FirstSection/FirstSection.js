980ca3940a54c9218d6341803b93ccb8a04ed4b4
Oleksandr <qwertyuiop.ua@gmail.com>
2021-08-03 18:14:10 +0300

    commit

--- src/container/Main/FirstSection/FirstSection.js
+++ src/container/Main/FirstSection/FirstSection.js
@@ -10,16 +10,45 @@ const FirstSection = () => {
                 display="flex"
                 maxWidth="1200px"
                 margin="0 auto"
+                justifyContent="space-between"
+                flex="0 1 100%"
             >
-                <Box>
-                    <div className="rental-item">
-                        <div className="rental-link">Equipment rental</div>
-                        <div className="rental-text">Sed ultrices magna</div>
+                <Box flex="0 1 100%">
+                    <div className="first-collum column">
+                        <div className="rental-item">
+                            <div className="link">Equipment rental</div>
+                            <div className="text">Sed ultrices magna</div>
+                        </div>
+
+                        <div className="shop-item">
+                            <img src="./images/logo.png" />
+                            <div className="link">Our shop</div>
+                        </div>
                     </div>
                 </Box>
-                <Box>
-                    <div className="shop-item">
-                        <div className="rental-link">Our shop</div>
+                <Box flex="0 1 100%">
+                    <div className="second-collum column">
+                        <div className="lessons-item">
+                            <div className="link">
+                                Ski and Snowboard Lessons
+                            </div>
+                        </div>
+                    </div>
+                </Box>
+                <Box flex="0 1 100%">
+                    <div className="third-collum column">
+                        <div className="jumping-item">
+                            <div className="link">
+                                Ski <br /> jumping
+                            </div>
+                            <div className="text">Fusce lobortis vel</div>
+                        </div>
+
+                        <div className="resort-item">
+                            <div className="link str2192">
+                                Explore the resort
+                            </div>
+                        </div>
                     </div>
                 </Box>
             </Box>
